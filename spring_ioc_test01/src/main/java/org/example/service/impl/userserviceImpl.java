ccfd9d8082c31999c1ac93d101c00b262955bf4f
Heath Cliff <784361867@qq.com>
2023-11-18 18:58:22 +0800

    spring入门

---
+++ spring_ioc_test01/src/main/java/org/example/service/impl/userserviceImpl.java
@@ -0,0 +1,6 @@
+package org.example.service.impl;
+
+import org.example.service.userservice;
+
+public class userserviceImpl implements userservice {
+}
--- spring_ioc_test01/src/main/java/org/example/test/BeanFactoryTest.java
+++ spring_ioc_test01/src/main/java/org/example/test/BeanFactoryTest.java
@@ -1,15 +1,11 @@
-# springFramwork
-黑马听课笔记：https://www.bilibili.com/video/BV1rt4y1u7q5/?p=2&amp;spm_id_from=pageDriver&amp;vd_source=3a388d296ad5d4ec5d3aed0300c5ddc8
+package org.example.test;
+import org.example.service.userservice;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import sun.security.util.math.IntegerModuloP;
 
-## spring用Bean工厂实现java类管理
-1. 创建maven工程，pom文件中导入spring-context坐标
-2. 用xml文件记录spring要管理的类
-    ```
-   SpringBeanManage.xml文件中：
-    <bean id="UserserviceBean" class="org.example.service.impl.userserviceImpl"></bean>
-   ```
-3. 通过bean工厂类用bean
-    ```java
+public class BeanFactoryTest {
+    public static void main(String[] args) {
         //1.创建工厂类
         DefaultListableBeanFactory dfltBF = new DefaultListableBeanFactory();
         //2.创建加载bean配置（即“SpringBeanManage.xml”）的读取器类，用于读xml配置文件
@@ -18,6 +14,7 @@
         xmlBeanReader.loadBeanDefinitions("SpringBeanManage.xml");
         //4.工厂通过xml拿到bean
         userservice usBean = (userservice) dfltBF.getBean("UserserviceBean");
+
         System.out.println(usBean);
-   
-   ```
+    }
+}
